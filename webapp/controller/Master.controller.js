5d19adb471279aa40cdd2b03f2069bf16de9c9fb
Alejandro Hroncich <alehroncich@gmail.com>
2023-06-28 19:03:16 +0000

    Crea un json para mockear los datos de la lista

--- webapp/controller/Master.controller.js
+++ webapp/controller/Master.controller.js
@@ -1,15 +1,21 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/m/library"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, MLibrary) {
         "use strict";
+        var URLHelper = MLibrary.URLHelper;
 
         return Controller.extend("lvcrft.lovecraftcollection.controller.Master", {
             onInit: function () {
 
+            },
+
+            handleLinkObjectAttributePress : function (oEvent) {
+                URLHelper.redirect("https://es.wikipedia.org/wiki/Mitos_de_Cthulhu", true);
             }
         });
     });
